ddb73f99b6bb29a312ee38679596a55e9b3ba72d
Yassar Hammami <yassarhammami@gmail.com>
2022-05-27 02:52:17 +0300

    .

--- src/main/java/com/jazara/icu/zipkin/ZipkinApplication.java
+++ src/main/java/com/jazara/icu/zipkin/ZipkinApplication.java
@@ -3,9 +3,11 @@ package com.jazara.icu.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableZipkinStreamServer
+@EnableEncryptableProperties
 public class ZipkinApplication {
 
     public static void main(String[] args) {
